e30784e58a968d2923ee4a1805ea1c090699cd02
Thet Htoo Aung <ththtooaung.e@gmail.com>
2023-06-29 22:18:11 +0630

    author(store,update,destroy)

--- app/Http/Controllers/Api/AuthorController.php
+++ app/Http/Controllers/Api/AuthorController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Models\Author;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreAuthorRequest;
+use App\Http\Requests\UpdateAuthorRequest;
 use App\Http\Resources\AuthorResource;
+use Illuminate\Support\Str;
 
 class AuthorController extends Controller
 {
@@ -20,9 +23,11 @@ class AuthorController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(StoreAuthorRequest $request)
+    {   
+        $author = $request->validated();
+        $author['slug'] = Str::slug($request->input('name'));
+        return new AuthorResource(Author::create($author));
     }
 
     /**
@@ -36,16 +41,18 @@ class AuthorController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateAuthorRequest $request, Author $author)
     {
-        //
+        $data = $request->validated();
+        $data['slug'] = Str::slug($request->input('name'));
+        return $author->update($data);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Author $author)
     {
-        //
+        return $author->delete();
     }
 }
